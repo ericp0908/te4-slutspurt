4a4e35003722974134cfbdc365431897523fb942
ericp0908 <eric.persson@elev.ga.ntig.se>
2019-04-25 15:10:47 +0200

    Lade till databas, började disgna hemsidan

---
+++ website/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Coronet</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="./style.css">
+    <title>Coronet</title>
+</head>
+<body>
+    <div class="main">
+        
+        <div class="logo">
+            <img src="pictures/coronet_logo.png" alt="logo" id="logo">
+        </div>
+
+        <div class="nav_bar">
+            <button type="button" id="bttn">Start</button>
+            <button type="button" id="bttn">Gallery</button>
+            <button type="button" id="bttn">About</button>
+            <button type="button" id="bttn">Contact</button>
+        </div>
+
+        <div class="body">
+        </div>
+
+    </div>
+
+
+</body>
+</html>
